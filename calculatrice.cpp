bd0153f61a4a400731a523c37de47ad37c982908
andreessam <96079505+andreessam@users.noreply.github.com>
2022-04-11 21:24:47 +0100

    calculatrice
    une simple calculatrice en C++

---
+++ calculatrice.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+int main(int argc, char** argv)
+{
+    int a,b;
+    int som, diff, pro, quo, reste;
+    cout<<"donnez la valeur de a:";
+    cin>>a;
+    cout<<"donnez la valeur de b:";
+    cin>>b;
+    som=a+b;
+    diff=a-b;
+    pro=a*b;
+    quo=a/b;
+    reste=a b;
+    cout<<"La somme est "<<som<<endl;;
+    cout<<"La difference est "<<diff<<endl;;
+    cout<<"Le produit est "<<pro<<endl;;
+    cout<<"le quotient est "<<quo<<endl;;
+    cout<<"le reste est "<<reste<<endl;;
+    system("pause");
+
+
+    
+    return 0;
+}
